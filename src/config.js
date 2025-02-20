8c849b3020bcb6b2ba5cef44cd532d8f8728895b
AMOUSSOU <amslubfonGeneration>
2025-02-20 02:04:51 +0100

    new  commit

--- src/config.js
+++ src/config.js
@@ -4,5 +4,5 @@ export const email_user = process.env.EMAIL_USER
 export const email_pass = process.env.EMAIL_PASS
 export const supabase_url = process.env.SUPABASE_URL
 export const supabase_key = process.env.SUPABASE_KEY
-export const session_key1 = process.env.SESSION_KEY1
-export const session_key2 = process.env.SESSION_KEY2
+export const session_key1 = Buffer.from(process.env.SESSION_KEY1, 'base64')
+export const session_key2 = Buffer.from(process.env.SESSION_KEY2, 'base64')

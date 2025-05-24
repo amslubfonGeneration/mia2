c2df5e5f2f4ecd0d64c27993cb9aa17414db5a32
AMOUSSOU <amslubfonGeneration>
2025-05-24 14:39:06 +0100

    new commit

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ import { session_key1, session_key2 } from "./config.js"
 import { EtuconnectPost, etuInscriptionPost, Information } from "./postaction_2.js"
 const host = ("RENDER" in process.env) ? `0.0.0.0` : `localhost`;
 
-const isMaintenanceMode = true; // Changez à false lorsque le site est opérationnel
+const isMaintenanceMode = false; // Changez à false lorsque le site est opérationnel
 const rootDir = dirname(dirname(fileURLToPath(import.meta.url)))
 const app = fastify({logger:true})
 app.register(fastifyView,{
